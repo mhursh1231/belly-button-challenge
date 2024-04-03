7b535cc3f8ae60d540035099b0f2c8fd2386dd54
mhursh1231 <mhursh1231@gmail.com>
2024-04-03 19:46:03 -0400

    Loaded in JSON data

--- README.md
+++
@@ -1,2 +0,0 @@
-# belly-button-challenge
-UPENN Module 14 Challenge
---
+++ static/js/app.js
@@ -0,0 +1,5 @@
+// Fetch the JSON data and console log it
+d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then(function(data) {
+    console.log(data);
+});
+
